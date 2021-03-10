b8ad5e3ba48d1f72bd77b976b3d11c7794a83ffd
ajexks284 <76217648+ajexks284@users.noreply.github.com>
2021-03-10 18:54:35 +0200

    Add scroll on menu page. Add function for updating the selected tab

--- src/index.js
+++ src/index.js
@@ -1,4 +1,60 @@
-import { createHeader, createHomeSection } from './page.js';
+import { createHeader } from './header.js';
+import { createHomeSection } from './home.js';
+import { createMenuSection } from './menu.js';
+import { createContactSection } from './contact.js';
 
+// All tabs will have the header
 createHeader();
+
+const content = document.querySelector('#content');
+
+// The section the tabs will be modifying
+const mainSection = document.createElement('section');
+mainSection.classList = 'main-section';
+content.appendChild(mainSection);
+
+// Add tab switching events
+const tabs = document.querySelectorAll('.navbar-ul-li');
+tabs.forEach(tab => {
+    tab.addEventListener('click', (e) => {
+        // Reset class
+        mainSection.classList = 'main-section';
+
+        // Change tab
+        if (tab.classList.contains('home')) {
+            mainSection.innerHTML = '';
+            createHomeSection();
+        };
+        if (tab.classList.contains('menu')) {
+            mainSection.innerHTML = '';
+            createMenuSection();
+        };
+        if (tab.classList.contains('contact')) {
+            mainSection.innerHTML = '';
+            createContactSection();
+        };
+
+        // Add scroll on menu page
+        if (mainSection.classList.contains('menu-section')) {
+            content.style.height = 'auto';
+            content.style.minHeight = '100vh'; 
+        } else {
+            content.style.height = '100vh';
+        }
+
+        // Update selected tab
+        updateSelectedTab(e);
+    });
+})
+
+function updateSelectedTab(e) {
+    tabs.forEach(tab => {
+        tab.classList.remove('selected');
+    })
+    e.target.classList.add('selected');
+}
+
+// By default we load the home section
 createHomeSection();
+
+export { mainSection }
